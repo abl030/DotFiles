eb6cc31ff00582c0ad782aca49ec7dff8e003f48
abl030 <abl030@gmail.com>
2024-06-24 22:05:32 +0800

    fixed the gcc error

--- NVIM/install_nvim.sh
+++ NVIM/install_nvim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #install neovim
-sudo apt install neovim
+sudo apt install neovim ripgrep -y
+#install gcc and others to stop errors
+#https://stackoverflow.com/questions/75587679/neovim-treesitter-returning-no-c-complier-error
+sudo apt update && sudo apt install build-essential
+
 #install nvchad
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 
