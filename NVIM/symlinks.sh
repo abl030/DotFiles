a75416c6e56feb321790f17939fd2284540714ab
abl030 <abl030@gmail.com>
2024-06-25 22:13:25 +0800

    pushed ui custom config for NVChad

--- NVIM/symlinks.sh
+++ NVIM/symlinks.sh
@@ -9,3 +9,7 @@ ln -s /home/abl030/DotFiles/NVIM/plugins.lua ~/.config/nvim/lua/plugins/plugins.
 ln -s /home/abl030/DotFiles/NVIM/init.lua ~/.config/nvim/lua/init.lua
 #add in our vimrc custom config.
 echo "dofile(vim.fn.stdpath('config') .. '/lua/init.lua')" >> ~/.config/nvim/lua/options.lua
+#add in our ui custom config
+sed -i '$d' ~/.config/nvim/lua/chadrc.lua
+echo 'return vim.tbl_deep_extend("force", M, require "ui")' >> ~/.config/nvim/lua/chadrc.lua
+ln -s ~/DotFiles/NVIM/ui.lua ~/.config/nvim/lua/ui.lua
