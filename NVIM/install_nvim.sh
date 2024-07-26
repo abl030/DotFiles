5f4d1f743a4bc5bb1480a624e4c10a9e77175d65
abl030 <abl030@gmail.com>
2024-07-26 14:10:29 +0800

    added in zip for nvim

--- NVIM/install_nvim.sh
+++ NVIM/install_nvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#install neovim
-sudo apt install neovim ripgrep -y
+#install neovim, ripgrep for fuzzy search and zip for WSL2 based machines
+sudo apt install neovim zip ripgrep -y
 #install gcc and others to stop errors
 #https://stackoverflow.com/questions/75587679/neovim-treesitter-returning-no-c-complier-error
 sudo apt update && sudo apt install build-essential
