eaceabe90d5624f880bc50da48e1d68c2ae739e0
abl030 <abl030@gmail.com>
2024-07-28 14:56:53 +0800

    fixed init.lua, don't need the wedge now just append the main init.lua

--- NVIM/scripts/symlinks.sh
+++ NVIM/scripts/symlinks.sh
@@ -6,9 +6,9 @@
 #plugins galore
 ln -s /home/abl030/DotFiles/NVIM/plugins.lua ~/.config/nvim/lua/plugins/plugins.lua
 #our main lua config
-echo 'vim.cmd('source ~/vim.vim')' >> ~/.config/nvim/init.lua
+echo "vim.cmd('source ~/vim.vim')" >> ~/.config/nvim/init.lua
 #add in our vimrc custom config.
-echo "dofile(vim.fn.stdpath('config') .. '/lua/init.lua')" >> ~/.config/nvim/lua/options.lua
+#echo "dofile(vim.fn.stdpath('config') .. '/lua/init.lua')" >> ~/.config/nvim/lua/options.lua
 #add in our ui custom config
 sed -i '$d' ~/.config/nvim/lua/chadrc.lua
 echo 'return vim.tbl_deep_extend("force", M, require "ui")' >> ~/.config/nvim/lua/chadrc.lua
