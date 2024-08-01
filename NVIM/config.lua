1050ded857c790a8d7d9b77f241a8c1032afe77d
abl030 <abl030@gmail.com>
2024-08-01 14:31:31 +0800

    added in markdown.nvim

--- NVIM/config.lua
+++ NVIM/config.lua
@@ -19,3 +19,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "markdown", "markdown_inline" },
+})
